5054415e8da1b531b1975f86271702d5f43a823c
Christian Abraham Sánchez Sánchez <chrisnewgear2011@gmail.com>
2026-03-02 21:53:58 -0500

    Initial Commit

---
+++ iOSTest/iOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestApp.swift
+//  iOSTest
+//
+//  Created by Christian Abraham Sanchez on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSTestTests/iOSTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestTests.swift
+//  iOSTestTests
+//
+//  Created by Christian Abraham Sanchez on 2/3/26.
+//
+
+import Testing
+@testable import iOSTest
+
+struct iOSTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
